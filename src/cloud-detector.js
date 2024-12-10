77f6a66e33191e81d8d47e1001e6726749b08015
kelvingelder24 <kelvingelder24@gmail.com>
2024-12-10 16:46:14 +0000

    refactor: optimize use of canvas api for video processing

--- src/cloud-detector.js
+++ src/cloud-detector.js
@@ -1,34 +1,32 @@
 import content from "./index";
 
+const canvas = document.createElement("canvas");
+
+canvas.width = 50;
+canvas.height = 20;
+
+const ctx = canvas.getContext("2d");
+
 export default function detectCloud(video) {
-  const canvas = document.createElement("canvas");
-
-  canvas.width = 50;
-  canvas.height = 20;
-
-  const ctx = canvas.getContext("2d");
   ctx.drawImage(video, 50, 0, 50, 20, 0, 0, 50, 20);
   const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
   let globalBrightness = 0;
   for (let i = 0; i < imgData.length; i += 4) {
-    const red = imgData[i];
-    const green = imgData[i + 1];
-    const blue = imgData[i + 2];
-    const localBrightness = (red + green + blue) / 3;
-    globalBrightness += localBrightness;
+    globalBrightness += (imgData[i] + imgData[i + 1] + imgData[i + 2]) / 3;
   }
   const averageBrightness = globalBrightness / (imgData.length / 4);
 
   const displayBrightness = 1.4 - averageBrightness / 310;
 
-  new Promise((resolve) => {
-    setTimeout(resolve, 700);
-  }).then(() => {
+  setTimeout(() => {
     for (const element of content.children) {
-      element.style.filter = `brightness(${displayBrightness})`;
+      element.style.filter = `brightness(${Math.trunc(displayBrightness * 100) / 100})`;
     }
-  });
-  new Promise((resolve) => {
-    setTimeout(resolve, 500);
-  }).then(() => detectCloud(video));
+  }, 700);
+
+  setTimeout(() => {
+    detectCloud(video);
+  }, 500);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
